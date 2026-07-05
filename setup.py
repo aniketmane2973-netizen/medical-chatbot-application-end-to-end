c876417b7f668cf4966cda832664ef811ae9811d
Aniket Mane <aniketmane2973@gmail.com>
2026-07-05 15:29:42 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Aniket Mane",
+    author_email= "aniketmane2973@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
